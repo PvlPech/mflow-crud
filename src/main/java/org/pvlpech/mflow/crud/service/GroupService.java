eef4ef14e580f9a20b9e9493a0568273989d04ea
PvlPech <pvlpech@gmail.com>
2024-10-06 23:03:06 +0500

    GroupService refactoring

--- src/main/java/org/pvlpech/mflow/crud/service/GroupService.java
+++ src/main/java/org/pvlpech/mflow/crud/service/GroupService.java
@@ -1,7 +1,9 @@
 package org.pvlpech.mflow.crud.service;
 
+import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
 import io.quarkus.panache.common.Sort;
+import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -12,6 +14,7 @@ import org.pvlpech.mflow.crud.mapper.GroupPartialUpdateMapper;
 import org.pvlpech.mflow.crud.model.Group;
 import org.pvlpech.mflow.crud.model.User;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -24,14 +27,16 @@ public class GroupService {
     @Inject
     GroupPartialUpdateMapper groupPartialUpdateMapper;
 
+    @Inject
+    CategoryService categoryService;
+
     @WithTransaction
     public Uni<Group> create(Group group) {
         return Uni.createFrom().item(this.validate(group))
-            .flatMap(g -> User.<User>findById(g.getOwner().getId())
-                .onItem().ifNull().failWith(new NotFoundException("User not found with id: " + g.getOwner().getId()))
-                .flatMap(u -> u.addServedGroup(g))
-                .flatMap(u -> g.persist())
-            );
+            .call(groupToCreate -> User.<User>findById(groupToCreate.getOwner().getId())
+                .onItem().ifNull().failWith(new NotFoundException("User not found with id: " + groupToCreate.getOwner().getId()))
+                .flatMap(groupToCreateUser -> groupToCreateUser.addServedGroup(groupToCreate)))
+            .flatMap(groupToCreate -> groupToCreate.persist());
     }
 
     /**
@@ -43,46 +48,67 @@ public class GroupService {
      */
     private Group validate(Group group) {
         var violations = this.validator.validate(group);
-
         if ((violations != null) && !violations.isEmpty()) {
             throw new ConstraintViolationException(violations);
         }
-
         return group;
     }
+
+    private Uni<Group> swapOwner(Group group, User owner) {
+        if (owner != null && !owner.equals(group.getOwner())) {
+            return deleteOwnerReference(group)
+                .flatMap(currentGroup -> User.<User>findById(owner.getId()))
+                .onItem().ifNull().failWith(new NotFoundException("User not found with id: " + owner.getId()))
+                .flatMap(targetOwner -> targetOwner.addServedGroup(group))
+                .replaceWith(group);
+        }
+        return Uni.createFrom().item(group);
+    }
+
     @WithTransaction
     public Uni<Group> partialUpdate(Group group) {
         return Group.<Group>findById(group.getId())
-            .onItem().ifNotNull().transformToUni(groupInDb -> {
-                // change the owner case start
-                if (group.getOwner() != null && !group.getOwner().equals(groupInDb.getOwner())) {
-                    return groupInDb.getOwner().deleteServedGroup(groupInDb)
-                        .flatMap(u -> User.<User>findById(group.getOwner().getId()))
-                        .onItem().ifNull().failWith(new NotFoundException("User not found with id: " + group.getOwner().getId()))
-                        .flatMap(u -> u.addServedGroup(groupInDb))
-                        .replaceWith(groupInDb);
-                // change the owner case end
-                } else {
-                    return Uni.createFrom().item(groupInDb);
-                }
-            })
-            .onItem().ifNotNull().transform(g -> {
-                this.groupPartialUpdateMapper.mapPartialUpdate(group, g);
-                return g;
+            .onItem().ifNotNull().transformToUni(groupInDb -> swapOwner(groupInDb, group.getOwner()))
+            .onItem().ifNotNull().transform(groupInDb -> {
+                this.groupPartialUpdateMapper.mapPartialUpdate(group, groupInDb);
+                return groupInDb;
             })
             .onItem().ifNotNull().transform(this::validate);
+    }
+
+    private Uni<Group> deleteServedCategories(Group group) {
+        return group.getServedCategories()
+            .map(HashSet::new) //copy of the servedCategories set to avoid concurrent modifications
+            .onItem().transformToMulti(Multi.createFrom()::iterable)
+            .filter(servedCategory -> servedCategory.getParent() != null) //to delete only parent categories, the rest of categories will be deleted by hierarchy
+            .call(servedCategoryToDelete -> categoryService.delete(servedCategoryToDelete.getId()))
+            .collect().asList()
+            .replaceWith(group);
+    }
+
+    private Uni<Group> deleteOwnerReference(Group group) {
+        return Uni.createFrom().item(group.getOwner())
+            .flatMap(groupOwner -> groupOwner.deleteServedGroup(group))
+            .replaceWith(group);
+    }
+
+    private Uni<Group> deleteUserReferences(Group group) {
+        return group.getUsers()
+            .map(HashSet::new) //copy of the users set to avoid concurrent modifications
+            .onItem().transformToMulti(Multi.createFrom()::iterable)
+            .call(groupUser -> groupUser.deleteGroup(group))
+            .collect().asList()
+            .replaceWith(group);
     }
 
     @WithTransaction
     public Uni<Void> delete(Long id) {
         return Group.<Group>findById(id)
             .onItem().ifNull().failWith(new NotFoundException("Group not found with id: " + id))
-            .flatMap(Group::deleteAllServedCategories)
-            .flatMap(g -> Uni.createFrom().item(g.getOwner())
-                .flatMap(u -> u.deleteServedGroup(g))
-                .flatMap(u -> u.deleteGroup(g))
-                .flatMap(u -> Group.deleteById(id)))
-            .replaceWithVoid();
+            .flatMap(this::deleteServedCategories)
+            .flatMap(this::deleteOwnerReference)
+            .flatMap(this::deleteUserReferences)
+            .flatMap(PanacheEntityBase::delete);
     }
 
     @WithTransaction
@@ -98,21 +124,21 @@ public class GroupService {
     @WithTransaction
     public Uni<Void> addUser(Long id, Long userId) {
         return Group.<Group>findById(id)
-                .onItem().ifNull().failWith(new NotFoundException("Group not found with id: " + id))
-                .flatMap(g -> User.<User>findById(userId)
-                        .onItem().ifNull().failWith(new NotFoundException("User not found with id: " + userId))
-                        .flatMap(u -> u.addGroup(g))
-                        .replaceWithVoid());
+            .onItem().ifNull().failWith(new NotFoundException("Group not found with id: " + id))
+            .flatMap(group -> User.<User>findById(userId)
+                .onItem().ifNull().failWith(new NotFoundException("User not found with id: " + userId))
+                .flatMap(user -> user.addGroup(group))
+                .replaceWithVoid());
     }
 
     @WithTransaction
     public Uni<Void> deleteUser(Long id, Long userId) {
         return Group.<Group>findById(id)
             .onItem().ifNull().failWith(new NotFoundException("Group not found with id: " + id))
-            .flatMap(g -> User.<User>findById(userId)
-                    .onItem().ifNull().failWith(new NotFoundException("User not found with id: " + userId))
-                    .flatMap(u -> u.deleteGroup(g))
-                    .replaceWithVoid());
+            .flatMap(group -> User.<User>findById(userId)
+                .onItem().ifNull().failWith(new NotFoundException("User not found with id: " + userId))
+                .flatMap(user -> user.deleteGroup(group))
+                .replaceWithVoid());
     }
 
     @WithTransaction
